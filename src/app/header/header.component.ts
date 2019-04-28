eccc6f6c8b9a558549757e8055aa804b4578c067
amit <amityadav215@gmail.com>
2019-04-28 22:35:14 +0530

    code updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,30 @@ export class HeaderComponent implements OnInit {
 
   schoolNumber = '1.855.RIDEZUM';
 
+  btnValue = "Hide School Value";
+
+  schoolValue: boolean = true;
   
+
+  changeVlave(){
+    
+    this.schoolValue = !this.schoolValue;
+
+    if(!this.schoolValue) {
+      this.btnValue = "Show School Value";
+    }
+    else{
+      this.btnValue = "Hide School Value";
+    }
+  }
+
+  dummySlogan = "slogan goes here";
+
+  makeYourSlogan(event:Event){
+    this.dummySlogan = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit() {
+  }
 
 }
--- src/app/intro-panel/intro-panel.component.ts
+++ src/app/intro-panel/intro-panel.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPanelComponent implements OnInit {
 
+  mainTitle = `Your school’s one-stop <br> partner for safe  reliable <br/> student transportation`;
+  subTitle = `Contact Zūm about your school <br/> transportation needs`;
+
   constructor() { }
 
   ngOnInit() {
