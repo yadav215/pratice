d0daf5647ad71b5519039da98af6c10976a3ff21
amit <amityadav215@gmail.com>
2019-05-21 21:55:15 +0530

    work updTED

--- src/app/intro-panel/intro-panel.component.ts
+++ src/app/intro-panel/intro-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-intro-panel',
@@ -7,12 +7,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPanelComponent implements OnInit {
 
-  mainTitle = `Your school’s one-stop <br> partner for safe  reliable <br/> student transportation`;
-  subTitle = `Contact Zūm about your school <br/> transportation needs`;
+ 
+
+ productInfo:any = [
+   {
+     id: 1,
+     title: 'Screw Driver',
+     price: 400,
+     stock: 11,
+     color:"btn-primary"
+   },{
+    id: 1,
+    title: 'Nut Volt',
+    price: 200,
+    stock: 5,
+    color:"btn-primary"
+  },{
+    id: 3,
+    title: 'Resistor',
+    price: 78,
+    stock: 45,
+    color:"btn-primary"
+  },{
+    id: 4,
+    title: 'Tractor',
+    price: 20000,
+    stock: 1,
+    color:"btn-primary"
+  },{
+    id: 5,
+    title: 'Roller',
+    price: 62,
+    stock: 15,
+    color:"btn-primary"
+  }
+ ]
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngOnChanges() {
+  
+  }
+  color:any;
+  colorManage = [];
+
+  stockVlaue(index,val){
+
+    if (this.productInfo[index].stock > 10) {
+      this.productInfo[index].color ="green";
+
+      this.productInfo[index].stock = val;
+
+      console.log(val , this.productInfo[index].stock);
+     
+  } else  {
+    this.productInfo[index].color ="red";
+    this.productInfo[index].stock = val;
+
+    console.log(val , this.productInfo[index].stock);
+  }
+}
 
 }
