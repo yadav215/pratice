b6748df42ebead6a0613e1eef34305c5c7051762
yadav215 <saty@webcontentor.com>
2019-06-17 17:56:18 +0530

    routerwork updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { PartnerComponent } from './partner/partner.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-const routes: Routes = [];
+
+
+const routesLink: Routes = [
+  {
+    path:'intro', component:IntroComponent
+  },{
+    path:'partner',component:PartnerComponent
+  },{
+    path:'contact',component:ContactFormComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routesLink)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
